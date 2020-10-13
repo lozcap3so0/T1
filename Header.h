9a92d47c94e560c6863c279ae9a6c5973d12a64e
nhattuan99 <nhattuan1610@gmail.com>
2020-10-13 10:25:36 +0700

    update

--- Header.h
+++ Header.h
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
-float CVvaDTHinhVuong(float dai, float rong);
-float CVvaDTHinhChuNhat(float dai, float rong);
-float CVvaDTHinhTron(float dai, float rong);
+float CVHinhVuong(float dai, float rong);
+float DTHinhVuong(float dai, float rong);
+float CVHinhChuNhat(float dai, float rong);
+float DTHinhChuNhat(float dai, float rong);
+float CVHinhTron(float dai, float rong);
+float DTHinhTron(float dai, float rong);
--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,27 @@
 #include "Header.h"
 
 
-float CVvaDTHinhVuong(float dai, float rong)
+float CVHinhVuong(float dai, float rong)
 {
 
 }
-float CVvaDTHinhChuNhat(float dai, float rong)
+float DTHinhVuong(float dai, float rong)
 {
 
 }
-float CVvaDTHinhTron(float dai, float rong)
+float CVHinhChuNhat(float dai, float rong)
+{
+
+}
+float DTHinhChuNhat(float dai, float rong)
+{
+
+}
+float CVHinhTron(float dai, float rong)
+{
+
+}
+float DTHinhTron(float dai, float rong)
 {
 
 }
