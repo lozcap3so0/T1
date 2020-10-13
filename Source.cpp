907d4889c4e6c471e15ff9dda4dba89d8c001f82
trung99 <vutientrung0301@gmail.com>
2020-10-13 11:16:27 +0700

    them +,-,*,/

--- Source.cpp
+++ Source.cpp
@@ -1,27 +1,27 @@
-#include "Header.h"
-
-
-float CVHinhVuong(float dai, float rong)
-{
-
-}
-float DTHinhVuong(float dai, float rong)
-{
-
-}
-float CVHinhChuNhat(float dai, float rong)
-{
-	return (dai + rong) * 2;
-}
-float DTHinhChuNhat(float dai, float rong)
-{
-	return dai * rong;
-}
-float CVHinhTron(float dai, float rong)
-{
-
-}
-float DTHinhTron(float dai, float rong)
-{
-
-}
+//#include "Header.h"
+//
+//
+//float CVHinhVuong(float dai, float rong)
+//{
+//
+//}
+//float DTHinhVuong(float dai, float rong)
+//{
+//
+//}
+//float CVHinhChuNhat(float dai, float rong)
+//{
+//	return (dai + rong) * 2;
+//}
+//float DTHinhChuNhat(float dai, float rong)
+//{
+//	return dai * rong;
+//}
+//float CVHinhTron(float dai, float rong)
+//{
+//
+//}
+//float DTHinhTron(float dai, float rong)
+//{
+//
+//}
