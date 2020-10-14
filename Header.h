b108de65e49542ab7768618bf44cf1856f7641cf
Thuc924 <ngohongthai05051994@gmail.com>
2020-10-14 21:11:55 +0700

    abcdef

---
+++ Header.h
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+float CVHinhVuong(float dai, float rong);
+float DTHinhVuong(float dai, float rong);
+float CVHinhChuNhat(float dai, float rong);
+float DTHinhChuNhat(float dai, float rong);
+float CVHinhTron(float bankinh);
+float DTHinhTron(float bankinh);
